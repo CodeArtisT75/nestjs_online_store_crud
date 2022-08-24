657dc547acd4781fdf51fdc98eb5157e66ea5e68
CodeArtisT75 <pcstar7000@gmail.com>
2022-08-24 19:08:34 +0430

    chore: add logger and global exception-filters

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './lib/filters/http-exception.filter';
+import { UnhandledErrorExceptionFilter } from './lib/filters/unhandled-error-exception.filter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: UnhandledErrorExceptionFilter
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter
+    }
+  ]
 })
 export class AppModule {}
